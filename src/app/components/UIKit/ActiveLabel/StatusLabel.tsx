61a25909bb7c69e0c450dea23d4a4d9ada7dd599
Dmytro Bulakhov <bulakhovda@gmail.com>
2024-05-05 13:50:09 +0300

    refactor(): Refactored styles using clsx

--- src/app/components/UIKit/ActiveLabel/StatusLabel.tsx
+++ src/app/components/UIKit/ActiveLabel/StatusLabel.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react";
 
+import clsx from "clsx";
+
 export interface IStatusLabelProps {
   children: ReactNode;
   status: string;
+  disabled?: boolean;
 }
 
 export enum Status {
@@ -12,16 +15,19 @@ export enum Status {
   Suspended = "suspended",
 }
 
-function StatusLabel({ children, status }: IStatusLabelProps) {
+function StatusLabel({ children, status, disabled }: IStatusLabelProps) {
   return (
     <div
-      className={`inline-flex items-center px-3.5 py-1 rounded-3xl text-sm font-medium ${
-        (status === Status.Active && "text-green-700 bg-green-100") ||
-        (status === Status.NotActive && "text-red-700 bg-red-100") ||
-        (status === Status.Pending && "text-orange-700 bg-orange-100") ||
-        (status === Status.Suspended && "text-blue-700 bg-blue-100") ||
-        ""
-      }`}
+      className={clsx(
+        `inline-flex items-center px-3.5 py-1 rounded-3xl text-sm font-medium`,
+        status === Status.Active && "text-green-700 bg-green-100",
+        status === Status.NotActive && "text-red-700 bg-red-100",
+        status === Status.Pending && "text-orange-700 bg-orange-100",
+        status === Status.Suspended && "text-blue-700 bg-blue-100",
+        {
+          ["opacity-75 cursor-not-allowed"]: disabled,
+        }
+      )}
     >
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
       {children}
